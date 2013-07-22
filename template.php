5e1f104637af671a89fc457ae3e523318f20300b
mimie <karen@imperium.ph>
2013-07-22 19:18:25 +0800

    added all files

---
+++ template.php
@@ -0,0 +1,137 @@
+<html>
+<head>
+<!--<link rel="stylesheet" type="text/css" url="design.css">-->
+<style type="text/css">
+div{
+  padding:5px 10px 5px 10px;
+}
+#logo{
+ height:60px;
+ width:150px;
+}
+
+#title{
+  background-color:#FBB117;
+  text-align:center;
+  color:black;
+  font-weight:bold;
+  font-family:Courier New;
+  font-size: 110%;
+  letter-spacing:22px;
+}
+
+#info{
+  background-color:#C0C0C0;
+  color:black;
+  font-weight:bold;
+  font-family:Courier New;
+  font-size: 110%;
+  letter-spacing:4px;
+}
+
+table{
+  padding:4px;
+}
+</style> 
+<title>Incident Report</title>
+</head>
+<body>
+
+<div>
+ <img id="logo" src="globe_logo.png">
+</div>
+
+<div id="title">
+ INCIDENT REPORT
+</div>
+
+<div id="message">
+Dear Customer,<br>
+Please accept our apology for the downtime encountered on your subscribed service/s.
+<br><br>
+</div>
+
+<div id="info">
+CUSTOMER INFORMATION
+</div>
+<table>
+  <tr>
+   <td width="40%"><b>Ticket Number</b></td>
+   <td width="60%">: 1031250</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Incident Type</b></td>
+   <td width="60%">: Network Outage</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Site Affected</b></td>
+   <td width="60%">: BPI-MERCADO HOSPITAL BATANGAS</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Circuit ID</b></td>
+   <td width="60%">: IC-AAG-4720</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Reported by</b></td>
+   <td width="60%">: mgbgestoso@bpi.com.ph</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Fault Reported Date/Time</b></td>
+   <td width="60%">: Nov 11, 2012 / 02:33 AM</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Fault Closed Date/Time</b></td>
+   <td width="60%">: Nov 11, 2012 / 11:46 AM</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Outage Date/Time</b></td>
+   <td width="60%">: Nov 11, 2012 / 02:33 AM</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Outage Resolved Date/Time</b></td>
+   <td width="60%">: Nov 11, 2012 / 02:33 AM</td>
+  </tr>
+  <tr>
+   <td width="40%"><b>Outage Duration</b></td>
+   <td width="60%">: Nov 11, 2012 / 02:33 AM</td>
+  </tr>
+<table>
+
+<div id="info">
+FAULT HANDLED
+</div>
+<div>Describe the fault handled.</div>
+
+<div id="info">
+DEFINITION OF THE PROBLEM
+</div>
+<div>Here is the defintion of the problem.</div>
+
+<div id="info">
+CONTAINMENT/INTERIM ACTIONS
+</div>
+<div>Here is the interim actions.</div>
+
+<div id="info">
+REASON FOR OUTAGE
+</div>
+<div>Here is the reason for outage.</div>
+
+<div id="info">
+CORRECTIVE ACTIONS
+</div>
+<div>Here is the corrective actions.</div>
+
+<div id="info">
+PREVENTIVE ACTIONS
+</div>
+<div>Here is the preventive actions.</div>
+
+<div id="info">
+RESTORATION DETAILS
+</div>
+<div>Restoration Details.</div>
+
+</body>
+</head>
+</html>
