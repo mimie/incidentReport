996b398b3b405c6d7609d2472c1bfa443a1c3966
mimie <karen.convicto@yahoo.com>
2013-07-23 13:48:53 +0800

    added file for getSiteAffected

--- incidentReport/test/testFile.php
+++ incidentReport/test/testFile.php
@@ -9,6 +9,13 @@
   $type_id = $details["type_id"];
   $incidentType = getIncidentType($type_id);
   var_dump($incidentType);
- 
 
+  /*$string = "ABC (Test1)";
+  echo preg_replace("/\([^)]+\)/","",$string); // 'ABC '
+  */
+
+  $customerUserId = $details["customer_user_id"];
+  $siteAffected = getSiteAffected($customerUserId);
+  var_dump($siteAffected);
+  
 ?>
